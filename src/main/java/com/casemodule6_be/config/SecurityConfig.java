1d03e653fb207ba4611e5edf954778db10248155
DOTRUONGBACH <vuabongdapro@gmail.com>
2023-03-17 08:17:04 +0700

    update crudhost 17/3

--- src/main/java/com/casemodule6_be/config/SecurityConfig.java
+++ src/main/java/com/casemodule6_be/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().ignoringAntMatchers("/**");
-        http.authorizeRequests().antMatchers( "/login", "/register", "/account/**","/rooms/**","/bill/**","/sendMail/**")
+        http.authorizeRequests().antMatchers( "/login", "/register", "/account/**","/rooms/**","/bill/**","/sendMail/**","/post/**")
         .permitAll()
                 .anyRequest().authenticated();
                 // .and().csrf().disable().formLogin().loginPage("/login").permitAll();
