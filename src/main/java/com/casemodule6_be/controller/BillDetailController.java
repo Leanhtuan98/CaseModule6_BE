6ece5f627ffa6f4c6967f5d682f814b803cc22d7
hiepnguyenhuy <ks.huyhiep@gmail.com>
2023-03-20 17:23:04 +0700

    update amountDay

--- src/main/java/com/casemodule6_be/controller/BillDetailController.java
+++ src/main/java/com/casemodule6_be/controller/BillDetailController.java
@@ -13,12 +13,24 @@ import java.util.List;
 public class BillDetailController {
     @Autowired
     BillDetailService billDetailService;
+
     @GetMapping("/findByBillId")
     public List<BillDetail> findBillDetailByBillId(@RequestParam long billId) {
-      return   billDetailService.findBillDetailByBillId(billId);
+        return billDetailService.findBillDetailByBillId(billId);
     }
+
     @GetMapping("/schedule/{roomid}")
-    public List<BillDetail> findSchedule(@PathVariable long roomid){
+    public List<BillDetail> findSchedule(@PathVariable long roomid) {
         return billDetailService.findSchedule(roomid);
+    }
+
+    @PostMapping
+    public BillDetail save(@RequestBody BillDetail billDetail) {
+        return billDetailService.save(billDetail);
+    }
+
+    @GetMapping("/showall")
+    public List<BillDetail> showall() {
+        return billDetailService.showAll();
     }
 }
